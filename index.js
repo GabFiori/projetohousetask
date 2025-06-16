63f6afd226eb42a555e7d3a1a5370ec3e67a7f04
Gabriel Fiori <140767553+GabFiori@users.noreply.github.com>
2025-06-15 23:16:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const __dirname = path.dirname(__filename);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
